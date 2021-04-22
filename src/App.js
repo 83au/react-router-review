1a3d83d5054d33991b9bb0ca63433572f5f5827f
Beau Sherman <brsherman88@gmail.com>
2021-04-21 19:28:56 -0700

    Add nav

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Link } from 'react-router-dom';
 import About from './About';
 import Dog from './Dog';
 import Contact from './Contact';
@@ -7,6 +7,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <nav className="App-nav">
+        <Link to="/dog">Dog</Link>
+        <Link to="/contact">Contact</Link>
+        <Link to="/">About</Link>
+      </nav>
       <Switch>
         <Route exact path="/" component={About} />
         <Route exact path="/dog" component={Dog} />
